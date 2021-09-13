2366ecba769b66edaad025dc5aebe96f86fcc8ee
Phan Long Hieu <hieuflong@gmail.com>
2021-09-13 10:56:19 +0700

    Feature: Reset styles using normalize.css

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
   html, body {
     height: 100%;
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
