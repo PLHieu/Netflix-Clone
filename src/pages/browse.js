e46ae40dda00b5cc7872550237d9a7ccc0fcf2a5
Phan Long Hieu <hieuflong@gmail.com>
2021-09-19 22:27:32 +0700

    Refactor: Remove unessary hook in browse Page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,5 @@
-import { Redirect } from 'react-router-dom';
 import { BrowseContainer } from '../containers/browse.container';
-import useAuthListener from '../hooks/use-auth-listener';
 
-export default function Browse() {
-  const { user } = useAuthListener();
-
-  return user ? <BrowseContainer /> : <Redirect to="/signin" />;
+export default function Browse({ user }) {
+  return <BrowseContainer user={user} />;
 }
