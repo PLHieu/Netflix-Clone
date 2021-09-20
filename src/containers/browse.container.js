440e6c94378c4e85edb77b47657ac495a20d809a
Phan Long Hieu <hieuflong@gmail.com>
2021-09-20 07:44:31 +0700

    Feature: Add Loading

--- src/containers/browse.container.js
+++ src/containers/browse.container.js
@@ -1,56 +1,66 @@
 import { getAuth } from 'firebase/auth';
-import React, { useState } from 'react';
-import { Header } from '../components';
+import React, { useState, useEffect } from 'react';
+import { Header, Loading } from '../components';
 import * as ROUTES from '../constants/routes';
 import { SelectProfileContainer } from './profiles.container';
 
 export function BrowseContainer({ user }) {
   const [category, setCategory] = useState('series');
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
 
   function signOut() {
     const auth = getAuth();
     auth.signOut();
   }
 
-  return profile.displayName ? (
-    <Header>
-      <Header.Frame>
-        <Header.Group>
-          <Header.Logo src="../../images/icons/logo.svg" to={ROUTES.HOME} />
-          <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>
-            Series
-          </Header.TextLink>
-          <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>
-            Films
-          </Header.TextLink>
-        </Header.Group>
-        <Header.Group>
-          <Header.Search />
-          <Header.Profile>
-            <Header.Picture src="../../images/users/1.png" />
-            <Header.Dropdown>
-              <Header.Group>
-                <Header.TextLink>{user.displayName}</Header.TextLink>
-              </Header.Group>
-              <Header.Group>
-                <Header.TextLink onClick={() => signOut()}>Sign Out</Header.TextLink>
-              </Header.Group>
-            </Header.Dropdown>
-          </Header.Profile>
-        </Header.Group>
-      </Header.Frame>
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile]);
 
-      <Header.Feature>
-        <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
-        <Header.Text>
-          Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
-          City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
-          futile attempt to feel like he's part of the world around him.
-        </Header.Text>
-        <Header.PlayButton>Play</Header.PlayButton>
-      </Header.Feature>
-    </Header>
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading /> : <Loading.ReleaseBody />}
+      <Header>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo src="../../images/icons/logo.svg" to={ROUTES.HOME} />
+            <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>
+              Series
+            </Header.TextLink>
+            <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>
+              Films
+            </Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search />
+            <Header.Profile>
+              <Header.Picture src="../../images/users/1.png" />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => signOut()}>Sign Out</Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+            City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+            futile attempt to feel like he's part of the world around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
   );
